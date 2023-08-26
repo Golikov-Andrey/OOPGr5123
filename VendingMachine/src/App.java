f1d19376d345d74186589d338cfc6090f196a7de
Sergey Mochalov <sergeymochalov12@yandex.ru>
2023-08-26 14:24:04 +0300

    HW2

--- VendingMachine/src/App.java
+++ VendingMachine/src/App.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
-
 import Domen.Bottle;
+import Domen.HotDrink;
 import Domen.Product;
 import Services.CoinDispenser;
 import Services.Holder;
@@ -12,16 +12,24 @@ public class App {
         List<Product> assort = new ArrayList<Product>(); 
 
         Product item1 = new Product("Lays", 100);
-        Product item2 = new Product("cola", 50);
+        Product item2 = new Product("Cola", 50);
         Product item3 = new Bottle("Mineral water", 101, 1.5);
+        Product item4 = new HotDrink("Black tea", 50,80);
+        Product item5 = new HotDrink("Green tea", 55,75);
+        Product item6 = new HotDrink("Coffee Cappuccino", 120,70 );
+        Product item7 = new HotDrink("Coffee Espresso", 150, 70);
 
         assort.add(item1);
         assort.add(item2);
         assort.add(item3);
+        assort.add(item4);
+        assort.add(item5);
+        assort.add(item6);
+        assort.add(item7);
+
 
         Holder hold1 = new Holder(10, 10);
         CoinDispenser disp = new CoinDispenser(120);
-
         VendingMachine venMach1 = new VendingMachine(hold1, disp, assort);
 
         for(Product prod: venMach1.getProductList())
